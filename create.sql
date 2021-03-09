563c71a96fa078379500b5df55ffe736ca0cf531
Isaac23-K <ikaptoge@yahoo.com>
2021-03-09 18:21:36 +0300

    created database company news

---
+++ create.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE companynews;
+\c
+
+CREATE TABLE users ( id SERIAL PRIMARY KEY , name VARCHAR , position VARCHAR , role VARCHAR , department VARCHAR , departmentId INT);
+CREATE TABLE news(id SERIAL PRIMARY KEY, title VARCHAR, content VARCHAR, author VARCHAR, departmentId INT);
+CREATE TABLE departments(id SERIAL PRIMARY KEY, name VARCHAR, description VARCHAR, totalemployees INTEGER);
+
+CREATE DATABASE companynews_test WITH TEMPLATE companynews;
--- src/main/java/models/user.java
+++ src/main/java/models/user.java
@@ -1,4 +1,16 @@
 package models;
 
 public class user {
+    private int id;
+    private String username ;
+    private int department ;
+    private String role ;
+    private String position ;
+
+    public user(String username, int department, String role, String position){
+        this.department = department;
+        this.username = username ;
+        this.role = role;
+        this.position = position ;
+    }
 }
