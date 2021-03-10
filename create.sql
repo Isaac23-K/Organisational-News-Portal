9be601c0894e50d441f3eac695d6cbbf66326188
Isaac23-K <ikaptoge@yahoo.com>
2021-03-10 03:07:29 +0300

    made constructors for user

--- create.sql
+++ create.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE companynews;
 \c
 
-CREATE TABLE users ( id SERIAL PRIMARY KEY , name VARCHAR , position VARCHAR , role VARCHAR , department VARCHAR , departmentId INT);
+CREATE TABLE users( id SERIAL PRIMARY KEY , name VARCHAR , position VARCHAR , role VARCHAR , department VARCHAR , departmentId INT);
 CREATE TABLE news(id SERIAL PRIMARY KEY, title VARCHAR, content VARCHAR, author VARCHAR, departmentId INT);
 CREATE TABLE departments(id SERIAL PRIMARY KEY, name VARCHAR, description VARCHAR, totalemployees INTEGER);
 
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,19 +1,54 @@
 package models;
 import org.sql2o.*;
 
-import java.util.List;
-
 public class User {
     private int id;
     private String username ;
-    private int department ;
+    private int idDepartment ;
     private String role ;
     private String position ;
 
-    public User(String username, int department, String role, String position){
-        this.department = department;
+    public User(String username, int idDepartment, String role, String position){
+        this.idDepartment= idDepartment;
         this.username = username ;
         this.role = role;
         this.position = position ;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public int getIdDepartment() {
+        return idDepartment;
+    }
+    public void setIdDepartment(int idDepartment) {
+        this.idDepartment = idDepartment;
+    }
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+
+        this.position = position;
     }
 }
